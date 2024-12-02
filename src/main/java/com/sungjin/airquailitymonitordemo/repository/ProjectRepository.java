cf0ed86a02f5cd15abda46f86f3d4c941604da5e
sssungjin <dkdlwls576@gmail.com>
2024-12-02 15:57:48 +0900

    feat: search project by device id

--- src/main/java/com/sungjin/airquailitymonitordemo/repository/ProjectRepository.java
+++ src/main/java/com/sungjin/airquailitymonitordemo/repository/ProjectRepository.java
@@ -4,7 +4,9 @@ import com.sungjin.airquailitymonitordemo.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-
+    Optional<Project> findByDevicesDeviceId(String deviceId);
 }
