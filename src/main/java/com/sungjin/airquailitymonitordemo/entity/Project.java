4d06f393508c77bbbbfb43a07101bf228ad64c69
sssungjin <dkdlwls576@gmail.com>
2024-12-03 12:58:12 +0900

    fix: project jpa lazy loading 에러 수정

--- src/main/java/com/sungjin/airquailitymonitordemo/entity/Project.java
+++ src/main/java/com/sungjin/airquailitymonitordemo/entity/Project.java
@@ -1,5 +1,6 @@
 package com.sungjin.airquailitymonitordemo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties({"sensorDataList", "devices"})
 public class Project implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
