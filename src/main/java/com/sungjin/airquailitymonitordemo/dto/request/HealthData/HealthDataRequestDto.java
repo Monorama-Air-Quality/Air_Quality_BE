7aece6eddf36aacb591bc283d626866be2c12241
nicolao00 <nicolao00@naver.com>
2024-12-11 10:22:00 +0900

    fix: 전송받을 헬스 데이터 dto 구조 변경

--- src/main/java/com/sungjin/airquailitymonitordemo/dto/request/HealthData/HealthDataRequestDto.java
+++ src/main/java/com/sungjin/airquailitymonitordemo/dto/request/HealthData/HealthDataRequestDto.java
@@ -1,11 +1,6 @@
 package com.sungjin.airquailitymonitordemo.dto.request.HealthData;
 
-import java.time.LocalDateTime;
-
 public record HealthDataRequestDto(
-    String email,
-    String provider,
     HealthUserInfoRequestDto userInfo,
-    MeasurementsRequestDto measurements,
-    LocalDateTime timestamp
+    MeasurementsRequestDto measurements
 ) {}
--- src/main/java/com/sungjin/airquailitymonitordemo/dto/request/HealthData/HealthUserInfoRequestDto.java
+++ src/main/java/com/sungjin/airquailitymonitordemo/dto/request/HealthData/HealthUserInfoRequestDto.java
@@ -1,5 +1,6 @@
 package com.sungjin.airquailitymonitordemo.dto.request.HealthData;
 
+import com.sungjin.airquailitymonitordemo.entity.enums.Provider;
 import java.time.LocalDate;
 
 import com.sungjin.airquailitymonitordemo.entity.HealthUser;
@@ -7,15 +8,19 @@ import com.sungjin.airquailitymonitordemo.entity.enums.BloodType;
 import com.sungjin.airquailitymonitordemo.entity.enums.Gender;
 
 public record HealthUserInfoRequestDto(
-    String bloodType,
-    String biologicalSex,
-    String birthDate
+        String email,
+        String provider,
+        String birthDate,
+        String bloodType,
+        String biologicalSex
 ) {
     public HealthUser toEntity() {
         return HealthUser.builder()
+                .email(email)
+                .provider(Provider.toProvider(provider))
+                .birthDate(LocalDate.parse(birthDate))
                 .bloodType(BloodType.toBloodType(bloodType))
                 .biologicalSex(Gender.toGender(biologicalSex))
-                .birthDate(LocalDate.parse(birthDate))
                 .build();
     }
 }
